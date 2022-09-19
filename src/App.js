9f30559e46ea323674c8c2491f3950b2b4890e2b
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-19 18:39:45 +0100

    Add custom css to NavBar and App components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // External
 
 // Internal
-import "./App.css";
+import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 
 function App() {
     return (
-        <div className="App">
+        <div className={styles.App}>
             <NavBar />
         </div>
     );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,9 +7,11 @@ import Navbar from "react-bootstrap/Navbar";
 // Internal
 import logo from "../assets/logo.png";
 
+import styles from "../styles/NavBar.module.css";
+
 const NavBar = () => {
     return (
-        <Navbar expand="md" fixed="top">
+        <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
                 <Navbar.Brand>
                     <img src={logo} alt="logo" height="45" />
