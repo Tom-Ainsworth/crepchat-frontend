01d8be35d05feafa92fb60c813b250e1f520c130
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-20 16:32:06 +0100

    Optimize bootstrap imports, Add handleChange and handleSubmit functions for sign up data. Add error handling for each form input

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,6 @@
 // External
 import axios from "axios";
 
-// Internal
-
 axios.defaults.baseURL = "http://127.0.0.1:8000/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
