256cab1fdd325139c89ebdf83510d8b265171c58
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-22 00:55:52 +0100

    Fix: Change baseURL in axios default to deployed site

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 // External
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/";
+axios.defaults.baseURL = "crepchat-api.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
