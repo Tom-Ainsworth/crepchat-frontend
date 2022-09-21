0446cdedbfe938e14bb01bfd6fd853f34ac053ab
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-21 21:14:48 +0100

    Remove unnecessary comment from axiosDefaults

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,5 +3,4 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://crepchat-api.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-// ! Uncomment this line when going to production!
 axios.defaults.withCredentials = true;
