3e28127a9860366b340c8a4842f13ad8a5589175
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-22 01:06:13 +0100

    Fix: BaseURL missing https from the front, causing it to send post requests from the wrong url

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 // External
 import axios from "axios";
 
-axios.defaults.baseURL = "crepchat-api.herokuapp.com/";
+axios.defaults.baseURL = "https://crepchat-api.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
