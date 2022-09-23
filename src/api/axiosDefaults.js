1255b66ca0996e32655e7503d00279542b78bc62
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-23 19:06:27 +0100

    Add axios interceptors and logic to handle access and refresh tokens on user login with axiosRes

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -5,3 +5,6 @@ axios.defaults.baseURL = "https://crepchat-api.herokuapp.com/";
 // axios.defaults.baseURL = "http://127.0.0.1:8000/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
--- src/context/CurrentUserContext.js
+++ src/context/CurrentUserContext.js
@@ -1,6 +1,10 @@
 // External
 import axios from "axios";
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+// Internal
+import { axiosRes } from "../api/axiosDefaults";
 
 export const CurrentUserContext = createContext();
 export const setCurrentUserContext = createContext();
@@ -10,9 +14,11 @@ export const useSetCurrentUser = () => useContext(setCurrentUserContext);
 export const CurrentUserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
 
+    const history = useHistory;
+
     const handleMount = async () => {
         try {
-            const { data } = await axios.get("dj-rest-auth/user/");
+            const { data } = await axiosRes.get("dj-rest-auth/user/");
             setCurrentUser(data);
         } catch (err) {
             // console.log(err);
@@ -22,6 +28,28 @@ export const CurrentUserProvider = ({ children }) => {
     useEffect(() => {
         handleMount();
     }, []);
+
+    useMemo(() => {
+        axiosRes.interceptors.response.use(
+            (response) => response,
+            async (err) => {
+                if (err.response?.status === 401) {
+                    try {
+                        await axios.post("/dj-rest-auth/toke/refresh/");
+                    } catch (err) {
+                        setCurrentUser((prevCurrentUser) => {
+                            if (prevCurrentUser) {
+                                history.push("/login");
+                            }
+                            return null;
+                        });
+                    }
+                    return axios(err.config);
+                }
+                return Promise.reject(err);
+            }
+        );
+    });
 
     return (
         <CurrentUserContext.Provider value={currentUser}>
