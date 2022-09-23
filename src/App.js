2495f8588d0f10e6b1ed81a5fad1072eeadaca01
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-23 20:10:43 +0100

    Style: Add conditional rendering to Main content in App.js to account for extra icons covering content

--- src/App.js
+++ src/App.js
@@ -8,12 +8,19 @@ import NavBar from "./components/NavBar";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import LogInForm from "./pages/auth/LoginInForm";
+import { useCurrentUser } from "./context/CurrentUserContext";
 
 function App() {
+    const currentUser = useCurrentUser();
+
     return (
         <div className={styles.App}>
             <NavBar />
-            <Container className={styles.Main}>
+            <Container
+                className={
+                    currentUser ? styles.MainLoggedIn : styles.MainLoggedOut
+                }
+            >
                 <Switch>
                     <Route exact path="/" render={() => <h1>Home</h1>} />
                     <Route exact path="/login" render={() => <LogInForm />} />
