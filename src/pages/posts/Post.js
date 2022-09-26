f8f3bc6b36d7698bd9f4100f7d6a4a2a67da3f65
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-26 20:38:26 +0100

    Add Post component to display an individual post by its id, including likes, dislikes and comments

---
+++ src/pages/posts/Post.js
@@ -0,0 +1,141 @@
+// External
+import Card from "react-bootstrap/Card";
+import Media from "react-bootstrap/Media";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import { Link } from "react-router-dom";
+
+// Internal
+import Avatar from "../../components/Avatar";
+import { useCurrentUser } from "../../context/CurrentUserContext";
+import styles from "../../styles/Post.module.css";
+
+const Post = (props) => {
+    const {
+        id,
+        owner,
+        profile_id,
+        profile_image,
+        comments_count,
+        likes_count,
+        dislikes_count,
+        like_id,
+        dislike_id,
+        caption,
+        category,
+        image,
+        updated_at,
+        postPage,
+    } = props;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
+    return (
+        <Card className={styles.Post}>
+            <Card.Body>
+                <Media className="align-items-center justify-content-between">
+                    <Link to={`/profiles/${profile_id}`}>
+                        <Avatar src={profile_image} height={55} />
+                        {owner}
+                    </Link>
+                    <div className="d-flex align-items-center">
+                        <span>
+                            {updated_at}
+                            {is_owner && postPage && "(dropdown Menu here)"}
+                        </span>
+                    </div>
+                </Media>
+            </Card.Body>
+            <Link to={`/posts/${id}`}>
+                <Card.Img src={image} alt={caption} />
+            </Link>
+            <Card.Body>
+                {caption && (
+                    <Card.Text className="text-center">{caption}</Card.Text>
+                )}
+                <div className={styles.PostBar}>
+                    {/* Likes logic and count */}
+                    {is_owner ? (
+                        <OverlayTrigger
+                            placement="top"
+                            overlay={
+                                <Tooltip>You can't like your own post!</Tooltip>
+                            }
+                        >
+                            <i className="fas fa-fire" />
+                        </OverlayTrigger>
+                    ) : like_id ? (
+                        <span onClick={() => {}}>
+                            <i className={`fas fa-fire ${styles.Fire}`} />
+                        </span>
+                    ) : currentUser ? (
+                        <span onClick={() => {}}>
+                            <i
+                                className={`fas fa-fire ${styles.FireOutline}`}
+                            />
+                        </span>
+                    ) : (
+                        <OverlayTrigger
+                            placement="top"
+                            overlay={<Tooltip>Log in to like posts!</Tooltip>}
+                        >
+                            <i className="fas fa-fire" />
+                        </OverlayTrigger>
+                    )}
+                    {likes_count}
+
+                    {/* Comments icon and count */}
+
+                    <Link to={`/posts/${id}`}>
+                        <i className="far fa-comments" />
+                    </Link>
+                    {comments_count}
+
+                    {/* Dislikes logic and count */}
+
+                    {is_owner ? (
+                        <OverlayTrigger
+                            placement="top"
+                            overlay={
+                                <Tooltip>
+                                    You can't dislike your own post!
+                                </Tooltip>
+                            }
+                        >
+                            <i className="fas fa-trash" />
+                        </OverlayTrigger>
+                    ) : dislike_id ? (
+                        <span onClick={() => {}}>
+                            <i className={`fas fa-fire ${styles.Fire}`} />
+                        </span>
+                    ) : currentUser ? (
+                        <span onClick={() => {}}>
+                            <i
+                                className={`fas fa-trash ${styles.FireOutline}`}
+                            />
+                        </span>
+                    ) : (
+                        <OverlayTrigger
+                            placement="top"
+                            overlay={
+                                <Tooltip>Log in to dislike posts!</Tooltip>
+                            }
+                        >
+                            <i className="fas fa-trash" />
+                        </OverlayTrigger>
+                    )}
+                    {dislikes_count}
+                </div>
+            </Card.Body>
+            {category && (
+                <Card.Title>
+                    <i className="fas fa-tag"></i>
+                    {category}
+                </Card.Title>
+            )}
+        </Card>
+    );
+};
+
+export default Post;
