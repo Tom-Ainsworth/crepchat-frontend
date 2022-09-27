5b32741bc09292968aa8db2a09179f3b4e1c8284
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-27 20:08:44 +0100

    Add text search functionality in PostsPage to search for users and captions

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,6 +1,6 @@
 // External
 import { useEffect, useState } from "react";
-// import Form from "react-bootstrap/Form";
+import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
@@ -8,7 +8,7 @@ import { useLocation } from "react-router-dom";
 
 // Internal
 import appStyles from "../../App.module.css";
-// import styles from "../../styles/PostsPage.module.css";
+import styles from "../../styles/PostsPage.module.css";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 import NoResults from "../../assets/no-results.jpeg";
@@ -19,10 +19,14 @@ function PostsPage({ message, filter = "" }) {
     const [hasLoaded, setHasLoaded] = useState(false);
     const { pathname } = useLocation();
 
+    const [query, setQuery] = useState("");
+
     useEffect(() => {
         const fetchPosts = async () => {
             try {
-                const { data } = await axiosReq.get(`/posts/?${filter}`);
+                const { data } = await axiosReq.get(
+                    `/posts/?${filter}search=${query}`
+                );
                 setPosts(data);
                 setHasLoaded(true);
             } catch (err) {
@@ -31,8 +35,14 @@ function PostsPage({ message, filter = "" }) {
         };
 
         setHasLoaded(false);
-        fetchPosts();
-    }, [filter, pathname]);
+        const timer = setTimeout(() => {
+            fetchPosts();
+        }, 1000);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [filter, query, pathname]);
 
     return (
         <Row className="h-100">
@@ -41,6 +51,20 @@ function PostsPage({ message, filter = "" }) {
             </Col>
             <Col className="py-2 p-0 p-lg-2" lg={8}>
                 <p>Popular profiles mobile</p>
+                <i className={`fas fa-search ${styles.SearchIcon} `} />
+                <Form
+                    className={styles.SearchBar}
+                    onSubmit={(event) => event.preventDefault()}
+                >
+                    <Form.Control
+                        type="text"
+                        className="mr-sm-2"
+                        placeholder="Search posts"
+                        value={query}
+                        onChange={(event) => setQuery(event.target.value)}
+                    />
+                </Form>
+
                 {hasLoaded ? (
                     <>
                         {posts.results.length ? (
