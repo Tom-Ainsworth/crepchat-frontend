d9a564521b586d968aef7f1e35e4b6933f96c4eb
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-28 18:43:52 +0100

    Add logic to delete comments in Comment.js, and pass in setComments and setPost props from PostPage.js

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,14 +1,51 @@
 // External
 import Media from "react-bootstrap/Media";
 import { Link } from "react-router-dom";
+import { axiosRes } from "../../api/axiosDefaults";
 import Avatar from "../../components/Avatar";
+import { MoreDropdown } from "../../components/MoreDropdown";
+import { useCurrentUser } from "../../context/CurrentUserContext";
 
 // Internal
 import styles from "../../styles/Comment.module.css";
 
 const Comment = (props) => {
-    console.log(props);
-    const { profile_id, profile_image, owner, updated_at, content } = props;
+    const {
+        profile_id,
+        profile_image,
+        owner,
+        updated_at,
+        content,
+        id,
+        setPost,
+        setComments,
+    } = props;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
+    const handleDelete = async () => {
+        try {
+            await axiosRes.delete(`/comments/${id}/`);
+            setPost((prevPost) => ({
+                results: [
+                    {
+                        ...prevPost.results[0],
+                        comments_count: prevPost.results[0].comments_count - 1,
+                    },
+                ],
+            }));
+
+            setComments((prevComments) => ({
+                ...prevComments,
+                results: prevComments.results.filter(
+                    (comment) => comment.id !== id
+                ),
+            }));
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     return (
         <div>
@@ -22,6 +59,12 @@ const Comment = (props) => {
                     <span className={styles.Date}>{updated_at}</span>
                     <p>{content}</p>
                 </Media.Body>
+                {is_owner && (
+                    <MoreDropdown
+                        handleEdit={() => {}}
+                        handleDelete={handleDelete}
+                    />
+                )}
             </Media>
         </div>
     );
