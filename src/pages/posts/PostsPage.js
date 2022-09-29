60f247baaf0bdb38676777c3da63e0fdf83ce505
Tom Ainsworth <tomainsworth94@gmail.com>
2022-09-29 16:39:21 +0100

    Add currentUser to PostsPage useEffect dependency array to refetch posts on user login/out

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -16,11 +16,13 @@ import NoResults from "../../assets/no-results.jpeg";
 import Asset from "../../components/Asset";
 import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function PostsPage({ message, filter = "" }) {
     const [posts, setPosts] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
     const { pathname } = useLocation();
+    const currentUser = useCurrentUser();
 
     const [query, setQuery] = useState("");
 
@@ -45,7 +47,7 @@ function PostsPage({ message, filter = "" }) {
         return () => {
             clearTimeout(timer);
         };
-    }, [filter, query, pathname]);
+    }, [filter, query, pathname, currentUser]);
 
     return (
         <Row className="h-100">
