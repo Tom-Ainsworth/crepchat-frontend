b5cccbd3bcf43d090005a868e6d6c6cb8ae472b5
Tom Ainsworth <tomainsworth94@gmail.com>
2023-03-01 11:51:01 +0000

    Add api baseURL for both prod and dev envs

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,12 @@
 // External
 import axios from "axios";
 
-axios.defaults.baseURL = "https://crepchat-api.onrender.com/";
+// UNCOMMENT WHEN DEPLOYING
+// axios.defaults.baseURL = "https://crepchat-api.onrender.com/";
+
+// COMMENT WHEN DEPLOYING
+axios.defaults.baseURL = "http://127.0.0.1:8000/";
+
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
