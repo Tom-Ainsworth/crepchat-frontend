12f34bd1023dfad7c21ef3b59554a44caf9c1e69
Tom Ainsworth <tomainsworth94@gmail.com>
2023-03-10 09:40:54 +0000

    Change axios defaults to live API

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -2,10 +2,10 @@
 import axios from "axios";
 
 // UNCOMMENT WHEN DEPLOYING
-// axios.defaults.baseURL = "https://crepchat-api.onrender.com/";
+axios.defaults.baseURL = "https://crepchat-api.onrender.com/";
 
 // COMMENT WHEN DEPLOYING
-axios.defaults.baseURL = "http://127.0.0.1:8000/";
+// axios.defaults.baseURL = "http://127.0.0.1:8000/";
 
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
