c7c5a9723aaf41fa1cea35b9b7e9db7902f10050
Tom Ainsworth <tomainsworth94@gmail.com>
2023-04-28 16:10:15 +0100

    Remove avatar style change

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,18 +3,18 @@ import Image from "next/image";
 import styles from "../styles/Avatar.module.css";
 
 const Avatar = ({ src, height = 45, text }) => {
-	return (
-		<span>
-			<Image
-				className={styles.Avatar}
-				src={src}
-				height={height}
-				width={height}
-				alt="avatar"
-			/>
-			{text}
-		</span>
-	);
+    return (
+        <span>
+            <Image
+                className={styles.Avatar}
+                src={src}
+                height={height}
+                width={height}
+                alt="avatar"
+            />
+            {text}
+        </span>
+    );
 };
 
 export default Avatar;
